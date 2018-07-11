14ed3d44f278aa4105cdeee2996677469a9566f1
bublyyz <31184516+bublyyz@users.noreply.github.com>
2018-07-11 16:57:25 +0530

    Update SampleTest2.java

--- pumainternalproject/src/test/java/com/tyss/test/SampleTest2.java
+++ pumainternalproject/src/test/java/com/tyss/test/SampleTest2.java
@@ -5,7 +5,7 @@ public class SampleTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
-		System.out.println("new");
+		System.out.println("github");
 	}
 
 }
